e3ca7128c95a59787c6692ed47ac5d6d135d8264
Listera <dudgns0721@gmail.com>
2025-06-13 16:29:21 +0900

    making translate(usersetting) + change language
    + change language
    + init korean translate file for working now

--- Assets/Scripts/JsonTranslate.cs
+++ Assets/Scripts/JsonTranslate.cs
@@ -32,12 +32,19 @@ public class TranslateData
 
 public class JsonTranslate : MonoBehaviour
 {
+    public GameObject languageDropBox;
+
     private TextMeshProUGUI[] targetObjects;
     private string[] targetTexts;
     private Dictionary<string, string> localizedText;
 
     void Awake()
     {
+        if (!languageDropBox)
+        {
+            Debug.LogWarning("LanguageDropBox is Null");
+        }
+        
         targetObjects = Resources.FindObjectsOfTypeAll<TextMeshProUGUI>();
         targetTexts = new string[targetObjects.Length];
 
@@ -46,12 +53,50 @@ public class JsonTranslate : MonoBehaviour
             targetTexts[i] = targetObjects[i].text;
         }
 
-        TranslateAllTexts();
+        SetLanguageInDropBox();
+        languageDropBox.GetComponent<TMP_Dropdown>().onValueChanged.AddListener(TranslateAllTexts);
     }
 
-    public void TranslateAllTexts()
+    public void SetLanguageInDropBox()
     {
-        LoadTranslateData("English");
+        if (!languageDropBox) return;
+
+        TMP_Dropdown dropdown = languageDropBox.GetComponent<TMP_Dropdown>();
+
+        string path = Application.dataPath + "/../Language/";
+        string[] filesName = Directory.GetFiles(path, "*.json", SearchOption.AllDirectories);
+
+        foreach (string name in filesName)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(name);
+            dropdown.options.Add(new TMP_Dropdown.OptionData(fileName));
+        }
+
+        for (int i = 0; i < dropdown.options.Count; i++)
+        {
+            if (dropdown.options[i].text == "English")
+            {
+                dropdown.value = i;
+                dropdown.RefreshShownValue();
+                break;
+            }
+        }
+    }
+
+    public void TranslateAllTexts(int index)
+    {
+        TMP_Dropdown dropdown = languageDropBox.GetComponent<TMP_Dropdown>();
+
+        string language = dropdown.options[index].text;
+        string path = Application.dataPath + "/../Language/" + language + ".json";
+
+        if (!File.Exists(path))
+        {
+            Debug.LogError("No File: " + path);
+            return;
+        }
+
+        localizedText = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(path));
 
         for (int i = 0; i < targetObjects.Length; i++)
         {
@@ -71,22 +116,8 @@ public class JsonTranslate : MonoBehaviour
                 targetObjects[i].text = targetTexts[i];
             }
         }
-    }
-
-    public void LoadTranslateData(string fileName)
-    {
-        string path = Application.dataPath + "/../Language/" + fileName + ".json";
-
-        if (!File.Exists(path))
-        {
-            Debug.LogError("No File: " + path);
-            return;
-        }
-        else
-        { 
-            Debug.Log(path + " is Loaded!");
-        }
-
-        localizedText = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(path));
+        
+        dropdown.value = index;
+        dropdown.RefreshShownValue();
     }
 }
