fea2ac49910f28556bb46f5a612cbefe3ce0ee11
Listera <dudgns0721@gmail.com>
2025-07-17 12:45:15 +0900

    finish making extended inventroy (inventory)

--- Assets/Scripts/Inventory/Extended Inventory.cs
+++ Assets/Scripts/Inventory/Extended Inventory.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -66,22 +67,62 @@ public class ExtendedInventory : MonoBehaviour, IInventorySystem
 
     public void AddItem(ItemData item, int index)
     {
-
+        if (index < inventoryInfo.inventoryItemData.Length)
+        { 
+            inventoryInfo.inventoryItemData[index] = item;
+            ShowInventory();
+        } 
     }
 
     public void RemoveItem(ItemData item)
     {
+        int index = FindItemIndex(item, true);
 
+        if (index != -1)
+        {
+            int invItemCount = inventoryInfo.inventoryItemData[index].itemCount;
+
+            if (invItemCount >= item.itemCount)
+            {
+                inventoryInfo.inventoryItemData[index].itemCount -= item.itemCount;
+            }
+            else
+            {
+                item.itemCount -= invItemCount;
+                inventoryInfo.inventoryItemData[index] = ItemDataBase.instance.NullItem();
+                RemoveItem(item);
+            }
+
+            SortSlot();
+            ShowInventory();
+        }
     }
 
     public void RemoveItemFromSlot(int index, int count)
     {
+        if (index < inventoryInfo.inventoryItemData.Length)
+        { 
+            if (count == -1)
+            {
+                inventoryInfo.inventoryItemData[index] = ItemDataBase.instance.NullItem();
+                return;
+            }
 
+            inventoryInfo.inventoryItemData[index].itemCount -= count;
+            if (inventoryInfo.inventoryItemData[index].itemCount <= 0)
+            {
+                inventoryInfo.inventoryItemData[index] = ItemDataBase.instance.NullItem();
+            }
+            ShowInventory();
+        } 
     }
 
     public void MoveItemToSlot(int fromIndex, int toIndex)
     {
-
+        // not work
+        ItemData item = inventoryInfo.inventoryItemData[fromIndex];
+        inventoryInfo.inventoryItemData[toIndex] = item;
+        inventoryInfo.inventoryItemData[fromIndex] = ItemDataBase.instance.NullItem();
     }
 
     public void ShowInventory()
@@ -159,6 +200,20 @@ public class ExtendedInventory : MonoBehaviour, IInventorySystem
         return -1;
     }
 
+    public void SortSlot()
+    {
+        for (int i = 0; i < inventoryInfo.inventoryItemData.Length; i++)
+        {
+            if (inventoryInfo.inventoryItemData[i].itemCount < 1)
+            {
+                for (int j = i; j < inventoryInfo.inventoryItemData.Length - 1; j++)
+                {
+                    inventoryInfo.inventoryItemData[j] = inventoryInfo.inventoryItemData[j + 1];
+                }
+            }
+        }
+    }
+
     public void ExtendSlot(bool extended)
     {
         int prevSize = inventoryInfo.inventoryItemData.Length;
@@ -174,7 +229,7 @@ public class ExtendedInventory : MonoBehaviour, IInventorySystem
 
         Transform inventoryObj = transform.Find("Inventory Obj");
         int slotCount = inventoryObj.childCount;
-        
+
         if (slotCount < newSize)
         {
             for (int i = slotCount; i < newSize; i++)
